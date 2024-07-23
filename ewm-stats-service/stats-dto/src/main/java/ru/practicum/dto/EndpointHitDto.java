3805eed261e0481cf7cffbe8c41ea0c365fea829
Jack <evgepuschckin@yandex.ru>
2024-07-23 14:00:28 +0300

    Create modules hierarchy

---
+++ ewm-stats-service/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -0,0 +1,22 @@
+package ru.practicum.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Builder
+public class EndpointHitDto {
+    private Long id;
+    @NotBlank
+    private String app;
+    @NotBlank
+    private String uri;
+    @NotBlank
+    private String ip;
+    @NotBlank
+    private String timestamp;
+}
